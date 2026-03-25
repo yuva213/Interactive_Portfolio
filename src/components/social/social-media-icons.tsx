6ef508ab56c8eab2a0dfd08a31ce9705e06cd4bb
Akuttyprince <yuvarajpro213@gmail.com>
2026-03-25 12:46:17 +0530

    adjust config

---
+++ src/components/social/social-media-icons.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { useInView } from "framer-motion";
+import React, { useRef } from "react";
+import { Button } from "../ui/button";
+import { SiGithub, SiInstagram, SiLinkedin, SiX } from "react-icons/si";
+import { config } from "@/data/config";
+import Link from "next/link";
+
+const BUTTONS = [
+  {
+    name: "Github",
+    href: config.social.github,
+    icon: <SiGithub size={"24"} color={"#fff"} />,
+  },
+  {
+    name: "LinkedIn",
+    href: config.social.linkedin,
+    icon: <SiLinkedin size={"24"} color={"#fff"} />,
+  },
+  {
+    name: "Twitter",
+    href: config.social.twitter,
+    icon: <SiX size={"24"} color={"#fff"} />,
+  },
+  {
+    name: "Instagram",
+    href: config.social.instagram,
+    icon: <SiInstagram size={"24"} color={"#fff"} />,
+  },
+];
+
+const SocialMediaButtons = () => {
+  const ref = useRef<HTMLDivElement>(null);
+  const show = useInView(ref, { once: true });
+  return (
+    <div ref={ref} className="z-10">
+      {show &&
+        BUTTONS.map((button) => (
+          <Link href={button.href} key={button.name} target="_blank">
+            <Button variant={"ghost"}>{button.icon}</Button>
+          </Link>
+        ))}
+    </div>
+  );
+};
+
+export default SocialMediaButtons;
