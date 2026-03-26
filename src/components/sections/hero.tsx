2a1e122a9fd9470afd08532322d6ec466f6fa0d3
Akuttyprince <yuvarajpro213@gmail.com>
2026-03-26 11:38:23 +0530

    performance boost

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -83,7 +83,7 @@ const HeroSection = () => {
               <div className="mt-8 flex flex-col gap-3 w-fit">
                 <Link
                   href={
-                    "https://drive.google.com/file/d/1O97WCk2DrO9x6SHOqf7LvRbmHkMgGIb4/view?usp=sharing"
+                    "https://drive.google.com/file/d/1emj-G5rRFEPyjBpr2hxgE1mGr6xIPXYH/view?usp=sharing"
                   }
                   target="_blank"
                   className="flex-1"
