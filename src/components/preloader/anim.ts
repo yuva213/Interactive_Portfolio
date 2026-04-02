ca36bb94c9a9ba4b73c4f01e366132da1031e49a
Akuttyprince <yuvarajpro213@gmail.com>
2026-04-02 09:48:25 +0530

    fix typo

--- src/components/preloader/anim.ts
+++ src/components/preloader/anim.ts
@@ -1,4 +1,6 @@
-export const opacity = {
+import { Variants } from "framer-motion";
+
+export const opacity: Variants = {
   initial: {
     opacity: 0
   },
@@ -8,12 +10,12 @@ export const opacity = {
   }
 };
 
-export const slideUp = {
+export const slideUp: Variants = {
   initial: {
     top: 0
   },
   exit: {
     top: '-100dvh',
-    transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.2 }
+    transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] as const, delay: 0.2 }
   }
 };
