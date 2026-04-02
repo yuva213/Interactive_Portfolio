c1fc9a3d54f471d2e1cb40820e0d57ebc7c8af32
Akuttyprince <yuvarajpro213@gmail.com>
2026-04-02 09:48:24 +0530

    fix responsive design

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    turbopack: {
-      // Set to '.' to ensure it uses the local folder as root and avoids picking up 
-      // global lockfiles from the user's home folder.
-      root: '.'
-    }
-  }
 };
 
 export default nextConfig;
