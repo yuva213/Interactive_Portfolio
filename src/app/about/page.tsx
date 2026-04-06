d5123bb6215fc7db43c27b5eb6893b9107dc54fe
Akuttyprince <yuvarajpro213@gmail.com>
2026-04-06 14:06:25 +0530

    update fonts

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -43,26 +43,20 @@ import { TbTerminal2 } from "react-icons/tb";
 const CONTACT_LINKS = [
   {
     name: "Email",
-    content: "naresh.khatri2345@gmail",
-    href: "mailto:naresh.khatri2345@gmail.com",
+    content: "yuvarajpro213@gmail",
+    href: "mailto:yuvarajpro213@gmail.com",
     icon: <FaEnvelope height={"50px"} />,
   },
   {
-    name: "Phone",
-    content: "1234567890",
-    href: "tel:1234567890",
-    icon: <FaPhone height={"50px"} />,
-  },
-  {
     name: "LinkedIn",
-    href: "https://www.linkedin.com/in/naresh-khatri/",
-    content: "/naresh-khatri",
+    href: "https://www.linkedin.com/in/yuva-raj-s-005419299/",
+    content: "/yuva-raj-s",
     icon: <FaLinkedin height={"50px"} />,
   },
   {
     name: "GitHub",
-    href: "https://github.com/Naresh-Khatri",
-    content: "/naresh-khatri",
+    href: "https://github.com/yuva213",
+    content: "/yuva213",
     icon: <FaGithub height={"50px"} />,
   },
 ];
@@ -251,7 +245,7 @@ function Page() {
                 />
               </div>
               <div className="flex flex-col gap-3 lg:items-center ml-10 md:ml-20 lg:ml-0">
-                <p className="text-center text-xl">Naresh Khatri</p>
+                <p className="text-center text-xl">Yuvaraj S</p>
                 <div className="text-xs bg-zinc-700 w-fit px-3 py-1 rounded-full">
                   Web Developer
                 </div>
@@ -287,16 +281,15 @@ function Page() {
           >
             <h1 className="text-3xl mb-10 lg:md-20">About me</h1>
             <p className="mb-10 text-roboto">
-              Hey there! I&apos;m Naresh, a Fullstack developer passionate about
-              creating meaningful digital experiences. With great in Web
-              development, I thrive on turning ideas into reality through coding
+              Hey there! I&apos;m Yuvaraj, a Fullstack developer passionate about
+              creating meaningful digital experiences. With a strong background in MERN 
+              stack development, I thrive on turning ideas into reality through coding
               and design. My journey began with a fascination for technology and
               a drive to make a positive impact.
             </p>
             <p className="mb-10">
-              When I&apos;m not coding, you can find me [Your
-              Interests/Hobbies], exploring new technologies, or sipping coffee
-              while brainstorming my next project.
+              When I&apos;m not coding, you can find me exploring Web3 and AI, 
+              participating in hackathons, or brainstorming my next project with a coffee.
             </p>
             <h1 className="text-3xl mb-10 lg:md-20">Stuff I use</h1>
             <div className="mb-5">
