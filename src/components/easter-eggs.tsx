30f093ea65be3f2f4d31c4cceed15979d604bccb
Akuttyprince <yuvarajpro213@gmail.com>
2026-04-06 14:06:02 +0530

    refactor components

--- src/components/easter-eggs.tsx
+++ src/components/easter-eggs.tsx
@@ -30,14 +30,14 @@ const EasterEggs = () => {
         "color: #FFD700; font-size: 16px; font-weight: bold; background-color: black; padding: 10px; border-radius: 10px;"
       );
 
-      ["naresh", "Naresh", "NARESH"].forEach((name) => {
+      ["yuvaraj", "Yuvaraj", "YUVARAJ"].forEach((name) => {
         // @ts-ignore
         if (Object.hasOwn(window, name)) return;
         Object.defineProperty(window, name, {
           get() {
             console.log(
               "%c✨ Abra Kadabra! ✨\n\n" +
-                "You just summoned the magic of Naresh! 🧙‍♂️\n" +
+                "You just summoned the magic of Yuvaraj! 🧙‍♂️\n" +
                 "What??? youre not impressed? Fine, but remember: With great power comes great responsibility! 💻⚡",
 
               "color: #FF4500; font-size: 18px; font-weight: bold; background-color: black; padding: 10px; border-radius: 10px; margin-top:10px"
