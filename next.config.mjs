6bcf9f8b7dbb0660961c37b80cfc8af7d719d95b
Akuttyprince <yuvarajpro213@gmail.com>
2026-04-07 13:44:37 +0530

    update routing

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.jsdelivr.net",
+      },
+    ],
+  },
   experimental: {
     turbopack: {
       root: "./",
