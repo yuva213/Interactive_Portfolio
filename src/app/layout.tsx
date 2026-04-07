dedeead2aa1ca63e3453315f9be42048e614b0f0
Akuttyprince <yuvarajpro213@gmail.com>
2026-04-07 13:44:37 +0530

    update documentation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,7 +66,7 @@ export default function RootLayout({
         ></Script>
         {/* <Analytics /> */}
       </head>
-      <body>
+      <body suppressHydrationWarning>
         <Providers>
           <Header />
           {children}
