b835c94efd6be4fbbe1a440ee21d689e09316e52
Akuttyprince <yuvarajpro213@gmail.com>
2026-04-07 14:35:56 +0530

    update fonts

--- src/components/ui/animated-modal.tsx
+++ src/components/ui/animated-modal.tsx
@@ -110,6 +110,7 @@ export const ModalBody = ({
 
           <motion.div
             ref={modalRef}
+            data-lenis-prevent="true"
             className={cn(
               "min-h-[50%] max-h-[90%] md:max-w-[40%] bg-white dark:bg-neutral-950 border border-transparent dark:border-neutral-800 md:rounded-2xl relative z-50 flex flex-col flex-1 overflow-hidden",
               className
