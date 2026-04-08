659db2d3fd3f234286a50cb7dedeb653d870728f
Akuttyprince <yuvarajpro213@gmail.com>
2026-04-08 09:58:58 +0530

    minor improvements

--- src/components/app-overlays.tsx
+++ src/components/app-overlays.tsx
@@ -5,6 +5,26 @@ import RemoteCursors from "@/components/realtime/remote-cursors";
 import EasterEggs from "@/components/easter-eggs";
 import ElasticCursor from "@/components/ui/ElasticCursor";
 import RadialMenu from "@/components/radial-menu/index";
+import { useEffect } from "react";
+import { useSounds } from "@/components/realtime/hooks/use-sounds";
+
+function GlobalClickSounds() {
+  const { playPressSound } = useSounds();
+
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      const target = e.target as HTMLElement;
+      if (target.closest('button') || target.closest('a') || target.closest('[role="button"]')) {
+        playPressSound();
+      }
+    };
+    
+    document.addEventListener("mousedown", handleClick);
+    return () => document.removeEventListener("mousedown", handleClick);
+  }, [playPressSound]);
+
+  return null;
+}
 
 export default function AppOverlays() {
   return (
@@ -17,6 +37,7 @@ export default function AppOverlays() {
       <EasterEggs />
       <ElasticCursor />
       <RadialMenu />
+      <GlobalClickSounds />
     </>
   );
 }
