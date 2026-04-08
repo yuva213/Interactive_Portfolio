85b7d6342fd11e55e676cb3633514ec78dd7ea78
Akuttyprince <yuvarajpro213@gmail.com>
2026-04-08 10:05:00 +0530

    small fixes

--- src/components/app-overlays.tsx
+++ src/components/app-overlays.tsx
@@ -37,7 +37,7 @@ export default function AppOverlays() {
       <EasterEggs />
       <ElasticCursor />
       <RadialMenu />
-      <GlobalClickSounds />
+      {/* <GlobalClickSounds /> */}
     </>
   );
 }
